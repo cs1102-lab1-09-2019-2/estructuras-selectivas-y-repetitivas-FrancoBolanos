1fb8fa118a62a420484aa858a5a07719c22bd972
FrancoBolanos <franco.bolanos@utec.edu.pe>
2019-08-30 21:43:28 -0500

    ejercicio6 resuelto

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,6 +1,23 @@
+#include <iostream>
 
+using namespace std;
 
 int main() {
-
+    int N;
+    cout<<"N: ";
+    cin>>N;
+    for (int i=1; i< N+1; ++i) {
+        for (int j=1; j < N+1; ++j) {
+            if (i%2==0){
+                if (i%2==0 and j%2==0) {
+                    cout << "#";
+                } else {
+                    cout << "@";
+                }} else {
+                if (i%2==0 or j%2==0) {
+                    cout << "@";
+                } else {
+                    cout << "#";
+                }}}cout << endl;}
     return 0;
 }
