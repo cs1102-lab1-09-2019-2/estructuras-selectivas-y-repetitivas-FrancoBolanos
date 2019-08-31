ca0768e2a648e1928eef58301817d29b64f18cce
FrancoBolanos <franco.bolanos@utec.edu.pe>
2019-08-30 20:45:22 -0500

    ejercicio5 resuelto

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,6 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
 
 
 int main() {
+    double N,xi,sumatoria=0,sumatoria_cuadrada=0, d_estandar;
+    int i=0;
 
+    cout<<"N: ";
+    cin>>N;
+    while (i<N) {
+        cout<<"x"<<i+1<<": ";
+        cin>>xi;
+        sumatoria+=xi;
+        sumatoria_cuadrada+=pow(xi,2);
+        i++;
+    }
+    d_estandar=sqrt((sumatoria_cuadrada/N)-(pow(sumatoria/N,2)));
+    cout<<"sd: "<<d_estandar<<"\n";
     return 0;
 }
