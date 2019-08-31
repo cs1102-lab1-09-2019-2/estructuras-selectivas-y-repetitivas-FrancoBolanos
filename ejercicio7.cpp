a6184150bb5fe698c2b68ede25b0ad148cbe9061
FrancoBolanos <franco.bolanos@utec.edu.pe>
2019-08-30 22:18:45 -0500

    ejercicio7 resuelto

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -1,7 +1,36 @@
+#include <iostream>
 
+using namespace std;
 
 int main() {
+    int P, autorizacion, CITV, SOAT;
 
+
+    do {
+    cout<<"Ingresar numero de identificaciòn vàlido: ";
+    cin>>P;
+    } while (P<=11111111 or P>=99999999);
+
+    autorizacion=P%100;
+    CITV=(P - autorizacion)/100%10;
+    SOAT=(P - CITV)/1000%10;
+
+    if (SOAT%2==0) {
+        cout<<"\nSOAT:Vigente\n";
+    } else {
+        cout<<"\nSOAT:Vencido\n";
+    }
+    if (CITV%2==0) {
+        cout<<"CITV:Vigente\n";
+    } else {
+        cout<<"CITV:Vencido\n";
+    }
+    if (autorizacion%3==0) {
+        cout<<"AUTORIZADO:Si\n";
+    } else {
+        cout<<"AUTORIZADO:No\n";
+    }
+    cout<<endl;
     return 0;
 }
 
